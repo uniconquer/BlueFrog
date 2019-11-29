a28d67be80cdc189522dfaae3c699cc26dd4405a
uniconquer <blueminy87@naver.com>
2019-11-30 04:23:33 +0900

    기본 윈도우 설정

--- BlueFrogEngine/WinMain.cpp
+++ BlueFrogEngine/WinMain.cpp
@@ -1,6 +1,35 @@
 #include <Windows.h>
-
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,	int nCmdShow)
 {
+	const auto pClassName = "BlueFrog";
+	
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+	
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0,
+		pClassName,
+		"Happy hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr);
+
+	// show window
+	ShowWindow(hWnd, SW_SHOW);
+
 	return 0;
 }
